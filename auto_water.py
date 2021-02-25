4c196983af30381d753eb40b9347542ec2352c1c
Arjen Hissink <appiekap653@hotmail.com>
2021-02-25 16:32:41 +0100

    Auto Water Test

---
+++ auto_water.py
@@ -0,0 +1,19 @@
+from gpiozero import *
+from signal import pause
+
+water_motor = DigitalOutputDevice(1)
+
+
+def auto_water_on(on_time, off_time):
+    water_motor.blink(on_time, off_time)
+
+
+def auto_water_off():
+    water_motor.off()
+    water_motor.close()
+
+
+auto_water_on(4, 240)
+auto_water_off()
+
+pause()
