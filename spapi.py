a959327dc1e954efce604636b36ba5247a3ba1d7
Arjen Hissink <appiekap653@hotmail.com>
2021-03-02 15:55:16 +0100

    Refactored

--- spapi.py
+++ spapi.py
@@ -1,34 +1,34 @@
-import RPi.GPIO as GPIO
-from signal import pause
+"""SpaPi: Sauna Control and Automatic Water Dispencer"""
 import time
 import sys
+import RPi.GPIO as GPIO
 
 GPIO.setwarnings(True)
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.OUT)
 
-time_on=20;
-parts=4;
-time_off=300;
-part_time=3;
+TIME_ON = 20
+PARTS = 4
+TIME_OFF = 300
+PART_TIME = 3
 
 try:
     while True:
         print("Water Running...")
-        for i2 in range(parts,0,-1):
+        for i2 in range(PARTS, 0, -1):
             GPIO.output(17, GPIO.HIGH)
-            for i in range(part_time,0,-1):
+            for i in range(PART_TIME, 0, -1):
                 sys.stdout.write(str(i)+' ')
                 sys.stdout.flush()
                 time.sleep(1)
-                
-            GPIO.output(17, GPIO.LOW)   
+
+            GPIO.output(17, GPIO.LOW)
             time.sleep(3)
-            
+
         print("Water Stopping...")
         GPIO.output(17, GPIO.LOW)
-        for i in range(time_off,0,-1):
+        for i in range(TIME_OFF, 0, -1):
             sys.stdout.write(str(i)+' ')
             sys.stdout.flush()
             time.sleep(1)
