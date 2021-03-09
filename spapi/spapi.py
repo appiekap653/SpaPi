fe4a85c184ca063cce0d21cc5d843cbf23cf078c
Arjen Hissink <appiekap653@hotmail.com>
2021-03-09 11:55:32 +0100

    added status and current segment properties

--- spapi/spapi.py
+++ spapi/spapi.py
@@ -3,7 +3,6 @@ SpaPi, Sauna and Automatic-Water control for RaspberryPi
 """
 import time
 
-from threading import Thread
 from water import controllers
 from water import scheme
 
@@ -11,9 +10,9 @@ CONTROLLER = controllers.WaterController(17)
 SCHEME = scheme.WaterScheme()
 
 SEGMENT1 = scheme.BurstSegment(3, 2, 2)
-SEGMENT2 = scheme.IdleSegment(600)
+SEGMENT2 = scheme.IdleSegment(1)
 SEGMENT3 = scheme.BurstSegment(6, 2, 4)
-SEGMENT4 = scheme.IdleSegment(300)
+SEGMENT4 = scheme.IdleSegment(1)
 
 SCHEME.add(SEGMENT1)
 SCHEME.add(SEGMENT2)
@@ -23,13 +22,15 @@ SCHEME.add(SEGMENT4)
 SCHEMERUNNER = scheme.SchemeRunner(CONTROLLER, SCHEME)
 
 try:
-    SCHEMERUNNER.start(True)
-    while SCHEMERUNNER.running:
+    SCHEMERUNNER.start(False)
+    while SCHEMERUNNER.status != scheme.RunnerStatus.Idle:
         pass
 
 except KeyboardInterrupt:
     pass
 finally:
+    print("stopping threads...")
     SCHEMERUNNER.stop()
+    print("cleuning up GPIO...")
     CONTROLLER.cleanup()
-    print("exiting...")
+    print("done and closed")
