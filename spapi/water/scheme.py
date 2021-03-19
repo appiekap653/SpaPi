b138ab494b60564fb190a9b0735b8b7bfab63d39
Arjen Hissink <appiekap653@hotmail.com>
2021-03-19 12:49:22 +0100

    added skip forward and backward

--- spapi/water/scheme.py
+++ spapi/water/scheme.py
@@ -15,17 +15,17 @@ class Segment(ABC):
     Segments.
     """
     @abstractmethod
-    def execute_segmemt(self, watercontroller: controllers.WaterController, runner: SchemeRunner) -> bool:
+    def execute_segmemt(self, watercontroller: controllers.WaterController, runner) -> bool:
         """Method to execute the segment"""
 
     @property
     @abstractmethod
-    def Type(self) -> str:
+    def segment_type(self) -> str:
         """Returns a string representation of the segment type"""
 
     @property
     @abstractmethod
-    def Data(self) -> str:
+    def data(self) -> str:
         """Returns a string representation of comma-seperated segment data"""
 
 class WaterSegment(Segment):
@@ -33,116 +33,147 @@ class WaterSegment(Segment):
     def __init__(self, duration: int):
         self._ontime = duration
 
-    def execute_segmemt(self, watercontroller: controllers.WaterController, runner: SchemeRunner) -> bool:
+    def execute_segmemt(self, watercontroller: controllers.WaterController, runner) -> bool:
         t_end = time.time() + self._ontime
 
         while time.time() < t_end:
             watercontroller.water_on()
-            if not runner._start:
+
+            if not runner._start or runner._skip_backward or runner._skip_forward:
                 break
 
         watercontroller.water_off()
         return True
 
     @property
-    def Type(self) -> str:
+    def segment_type(self) -> str:
         return self.__class__.__name__
 
     @property
-    def Data(self) -> str:
+    def data(self) -> str:
         data = 'Duration:{}'.format(self._ontime)
         return data
 
     @property
-    def Duration(self) -> int:
-        return self._ontime 
+    def duration(self) -> int:
+        return self._ontime
+
+    @duration.setter
+    def duration(self, value: int):
+        # Setting the duration
+        self._ontime = value
 
 class IdleSegment(Segment):
     """Idle segment to wait for a given duration"""
     def __init__(self, duration: int):
         self._ontime = duration
 
-    def execute_segmemt(self, watercontroller: controllers.WaterController, runner: SchemeRunner) -> bool:
+    def execute_segmemt(self, watercontroller: controllers.WaterController, runner) -> bool:
         t_end = time.time() + self._ontime
 
         while time.time() < t_end:
             watercontroller.water_off()
-            if not runner._start:
+
+            if not runner._start or runner._skip_backward or runner._skip_forward:
                 break
 
         return True
 
     @property
-    def Type(self) -> str:
+    def segment_type(self) -> str:
         return self.__class__.__name__
 
     @property
-    def Data(self) -> str:
+    def data(self) -> str:
         data = 'Duration:{}'.format(self._ontime)
         return data
 
     @property
-    def Duration(self) -> int:
+    def duration(self) -> int:
         return self._ontime
 
+    @duration.setter
+    def duration(self, value: int):
+        # Setting the duration
+        self._ontime = value
+
 class BurstSegment(Segment):
-    """Burst segment to run water for a given duration then stop 
+    """Burst segment to run water for a given duration then stop
     for a given duration and repeat for a specified number of times"""
     def __init__(self, duration_on: int, duration_off: int, repeat_number: int):
         self._ontime = duration_on
         self._offtime = duration_off
         self._repeatnumber = repeat_number
 
-    def execute_segmemt(self, watercontroller: controllers.WaterController, runner: SchemeRunner) -> bool:
+    def execute_segmemt(self, watercontroller: controllers.WaterController, runner) -> bool:
         for _ in range(self._repeatnumber, 0, -1):
             t_on = time.time() + self._ontime
 
             while time.time() < t_on:
                 watercontroller.water_on()
-                if not runner._start:
+
+                if not runner._start or runner._skip_backward or runner._skip_forward:
                     break
 
             t_off = time.time() + self._offtime
 
             while time.time() < t_off:
                 watercontroller.water_off()
-                if not runner._start:
+
+                if not runner._start or runner._skip_backward or runner._skip_forward:
                     break
 
-            if not runner._start:
+            if not runner._start or runner._skip_backward or runner._skip_forward:
                 break
 
         return True
 
     @property
-    def Type(self) -> str:
+    def segment_type(self) -> str:
         return self.__class__.__name__
 
     @property
-    def Data(self) -> str:
-        data = 'Duration_On:{},Duration_Off:{},Repeat_Number:{}'.format(self._ontime, self._offtime, self._repeatnumber)
+    def data(self) -> str:
+        data = 'Duration_On:{},Duration_Off:{},Repeat_Number:{}'.format(self._ontime,
+                                                                        self._offtime,
+                                                                        self._repeatnumber)
         return data
 
     @property
-    def Duration_On(self) -> int:
+    def duration_on(self) -> int:
         return self._ontime
 
-    @property
-    def Duration_Off(self) -> int:
-        return self._offtime
+    @duration_on.setter
+    def duration_on(self, value: int):
+        # Setting the duration
+        self._ontime = value
 
     @property
-    def Repeat_Number(self) -> int:
+    def duration_off(self) -> int:
+        return self._offtime
+
+    @duration_off.setter
+    def duration_off(self, value: int):
+        # Setting the duration
+        self._offtime = value
+
+    @property
+    def repeat_number(self) -> int:
         return self._repeatnumber
+
+    @repeat_number.setter
+    def repeat_number(self, value: int):
+        # Setting the duration
+        self._repeatnumber = value
 
 class WaterScheme:
     """Scheme that holds diferent segments to execute"""
     def __init__(self):
-        self._segments = list[Segment]
+        self._segments = []
 
     def __setitem__(self, pos: int, segment: Segment):
         if pos >= len(self._segments):
-	        raise IndexError('list index out of range')
+            raise IndexError('list index out of range')
 
         self._segments[pos] = segment
 
@@ -150,7 +181,7 @@ class WaterScheme:
         if pos >= len(self._segments):
             raise IndexError('list index out of range')
 
-        return self._segments[pos]    
+        return self._segments[pos]
 
     def add(self, segment: Segment):
         self._segments.append(segment)
@@ -158,7 +189,7 @@ class WaterScheme:
     def remove_at(self, pos: int):
         if pos >= len(self._segments):
             raise IndexError('list index out of range')
-        
+
         del self._segments[pos]
 
     def clear(self):
@@ -169,23 +200,27 @@ class WaterScheme:
         # Getting the segments
         return self._segments
 
-"""Representation of the current status of the SchemeRunner"""
+
 class RunnerStatus(Enum):
+    """Representation of the current status of the SchemeRunner"""
     Idle = 0
     Running = 1
     Paused = 2
 
-"""SchemeRunner to start/pause/resume a waterscheme"""
+
 class SchemeRunner:
+    """SchemeRunner to start/pause/resume a waterscheme"""
 
     def __init__(self, controller: controllers.WaterController, scheme: WaterScheme):
         self._controller = controller
         self._waterscheme = scheme
-        self._thread = Thread(target = self._thread_task)
+        self._thread = Thread(target=self._thread_task)
 
         self._pause = False
         self._start = False
         self._repeat = False
+        self._skip_forward = False
+        self._skip_backward = False
 
         self._status = RunnerStatus.Idle
         self._cur_segment_index = 0
@@ -202,7 +237,7 @@ class SchemeRunner:
         if self._start:
             self._start = False
             self._thread.join()
-        
+
     def start(self, repeat: bool = False):
         self._start = True
         self._status = RunnerStatus.Running
@@ -210,25 +245,44 @@ class SchemeRunner:
 
         self._thread.start()
 
+    def skip_forward(self) -> None:
+        if self._start:
+            self._skip_forward = True
+
+    def skip_backward(self) -> None:
+        if self._start:
+            self._skip_backward = True
+
     def _thread_task(self):
         while self._start:
-            for segment in self._waterscheme.segments:
+            i = 0
+            while i < len(self._waterscheme.segments):
+                segment = self._waterscheme.segments[i]
+
                 while self._pause is True:
                     self._status = RunnerStatus.Paused
                 else:
                     self._status = RunnerStatus.Running
+                    break
 
                 temp = thermometer.read_temp()
                 print('Temperature = {}'.format(temp))
 
                 self._cur_segment_index = self._waterscheme.segments.index(segment)
-                
+
                 print('Executing Segment: {}'.format(segment.Type), flush=True)
+
                 print('Segment Data: {}'.format(segment.Data), flush=True)
-                print('Current Index: {}'.format(self._waterscheme.segments.index(self.current_segment)))
-                print('Previous Index: {}'.format(self._waterscheme.segments.index(self.previous_segment)))
-                print('Next Index: {}'.format(self._waterscheme.segments.index(self.next_segment)))
-                
+
+                print('Current Index: {}'.format(self._waterscheme.segments.index(
+                    self.current_segment)))
+
+                print('Previous Index: {}'.format(self._waterscheme.segments.index(
+                    self.previous_segment)))
+
+                print('Next Index: {}'.format(self._waterscheme.segments.index(
+                    self.next_segment)))
+
                 segment.execute_segmemt(self._controller, self)
 
                 self._prev_segment_index = self._waterscheme.segments.index(segment)
@@ -236,13 +290,20 @@ class SchemeRunner:
                 if not self._start:
                     break
 
+                if self._skip_backward:
+                    i -= 1
+                    self._skip_backward = False
+                else:
+                    self._skip_forward = False
+                    i += 1
+
             if not self._repeat or not self._start:
                 self._cur_segment_index = 0
                 self._prev_segment_index = 0
                 self._status = RunnerStatus.Idle
                 self._start = False
                 return
-        
+
     @property
     def status(self) -> RunnerStatus:
         return self._status
@@ -257,15 +318,15 @@ class SchemeRunner:
 
         if next_segment_index < len(self._waterscheme.segments):
             return self._waterscheme.segments[next_segment_index]
-        else:
-            return self._waterscheme.segments[0]
+
+        return self._waterscheme.segments[0]
 
     @property
     def previous_segment(self) -> Segment:
         if self._prev_segment_index >= 0:
             return self._waterscheme.segments[self._prev_segment_index]
-        else:
-            return self._waterscheme.segments[0]
+
+        return self._waterscheme.segments[0]
 
     @property
     def waterscheme(self) -> WaterScheme:
@@ -275,7 +336,7 @@ class SchemeRunner:
     @waterscheme.setter
     def waterscheme(self, value: WaterScheme):
         # Setting the watherscheme
-        self._waterscheme=value
+        self._waterscheme = value
 
     @property
     def controller(self) -> controllers.WaterController:
@@ -285,4 +346,4 @@ class SchemeRunner:
     @controller.setter
     def controller(self, value: controllers.WaterController):
         # Setting the controller
-        self._controller=value
+        self._controller = value
